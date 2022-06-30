bc1a89231cbf4cec1d7016b865cb27e6c705c279
Medas <medas02345@gmail.com>
2022-06-30 15:31:47 +0300

    Add a playRound function

--- javascript.js
+++ javascript.js
@@ -12,4 +12,34 @@ function computerPlay(){
     }
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection){
+    let tempString = playerSelection.toLowerCase();
+    playerSelection = playerSelection[0].toUpperCase() + tempString.slice(1);
+
+    if(playerSelection === 'Rock' && computerSelection === 'Paper'){
+        return "You Lose! Paper beats rock!";
+    }
+    else if(playerSelection === 'Rock' && computerSelection === 'Scissors'){
+        return "You win! Rock beats scissors!";
+    }
+    else if(playerSelection === 'Paper' && computerSelection === 'Rock'){
+        return "You win! Paper beats rock!";
+    }
+    else if(playerSelection === 'Paper' && computerSelection === 'Scissors'){
+        return "You lose! Scissors beats paper!";
+    }
+    else if(playerSelection === 'Scissors' && computerSelection === 'Rock'){
+        return "You lose! Rock beats scissors!";
+    }
+    else if(playerSelection === 'Scissors' && computerSelection === 'Paper'){
+        return "You win! Scissors beats paper!";
+    }
+    else{
+        return "You both chose the same! It's a tie!";
+    }
+}
+/** 
+const playerSelection = 'Rock';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection,computerSelection));
+*/
